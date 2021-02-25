e07d2f0780f2a5485e8ca244d7a651977816fd0d
Hernan <hernan.grecco@gmail.com>
2021-02-25 01:48:14 -0300

    Added version

--- nbkanjit/__init__.py
+++ nbkanjit/__init__.py
@@ -12,6 +12,21 @@ from .exceptions import MissingAnnotation, NotNumbaSignature, UnknownAnnotation
 from .manager import JitManager
 from .signature import Function, anjit
 
+try:
+    from importlib.metadata import version
+except ImportError:
+    # Backport for Python < 3.8
+    from importlib_metadata import version
+
+try:  # pragma: no cover
+    __version__ = version("numbakit-anjit")
+except Exception:  # pragma: no cover
+    # we seem to have a local copy not installed without setuptools
+    # so the reported version will be unknown
+    __version__ = "unknown"
+
+del version
+
 __all__ = [
     "UnknownAnnotation",
     "NotNumbaSignature",
